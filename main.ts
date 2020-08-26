9c4429aaac7547fdba857f0e368bdf00a90fffea
Apitep <51409860+apitep@users.noreply.github.com>
2020-08-26 15:56:18 +0200

    added IR remote code

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
+IrRemote.onPressEvent(RemoteButton.NUM9, function () {
+    basic.showString("9")
+})
 IrRemote.onPressEvent(RemoteButton.NUM0, function () {
     basic.showString("0")
 })
+IrRemote.onPressEvent(RemoteButton.NUM7, function () {
+    basic.showString("7")
+})
+IrRemote.onPressEvent(RemoteButton.NUM8, function () {
+    basic.showString("8")
+})
 IrRemote.onPressEvent(RemoteButton.EQ, function () {
     basic.showString("-")
+})
+IrRemote.onPressEvent(RemoteButton.NUM3, function () {
+    basic.showString("3")
 })
 IrRemote.onPressEvent(RemoteButton.NUM1, function () {
     basic.showString("1")
@@ -23,6 +35,15 @@ IrRemote.onPressEvent(RemoteButton.A, function () {
         . . . . .
         `)
     magicbit.Servo(magicbit.Servos.S2, 0)
+})
+IrRemote.onPressEvent(RemoteButton.NUM6, function () {
+    basic.showString("6")
+})
+IrRemote.onPressEvent(RemoteButton.NUM5, function () {
+    basic.showString("5")
+})
+IrRemote.onPressEvent(RemoteButton.NUM2, function () {
+    basic.showString("2")
 })
 IrRemote.onPressEvent(RemoteButton.Left, function () {
     basic.showLeds(`
@@ -58,6 +79,9 @@ IrRemote.onPressEvent(RemoteButton.B, function () {
         . # . # .
         `)
     magicbit.Servo(magicbit.Servos.S2, 180)
+})
+IrRemote.onPressEvent(RemoteButton.NUM4, function () {
+    basic.showString("4")
 })
 IrRemote.onPressEvent(RemoteButton.Down, function () {
     basic.showLeds(`
@@ -101,8 +125,15 @@ IrRemote.onPressEvent(RemoteButton.C, function () {
 let stop = 0
 let arriere = 0
 let avant = 0
-IrRemote.init(Pins.P1)
+basic.showLeds(`
+    # . . . #
+    . # # # .
+    # . # . #
+    # . # . #
+    . # # # .
+    `)
 basic.showString("NILS")
+IrRemote.init(Pins.P1)
 avant = 180
 arriere = 0
 stop = 90
@@ -110,9 +141,9 @@ basic.forever(function () {
     if (input.buttonIsPressed(Button.AB)) {
         basic.showLeds(`
             # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
+            # # . # #
+            # . . . #
+            # # . # #
             # # # # #
             `)
         magicbit.MotorStopAll()
